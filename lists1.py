48ebb2fa584ca386be6082b71053a1eeee2769ca
Akash146759 <akash.ganguly13@gmail.com>
2025-04-05 16:26:40 +0530

    4/5/25

--- lists1.py
+++ lists1.py
@@ -1,6 +1,6 @@
 #Create an empty list
-empty_list = []
 print()
+
 
 # A list of numbers
 numbers = [1, 2, 3, 4, 5]
@@ -20,5 +20,5 @@ print(ra)
 #reverse the given list
 aList = [100, 200, 300, 400, 500]
 aList = aList[::-1]
-print(aList,"\n")
+print(aList,"\n",aList)
 
--- lists2.py
+++ lists2.py
@@ -5,15 +5,16 @@
 def match_words(words):
 	ctr = 0
 	lst = []
-	for word in words:
-		if len(word) > 1 and word[0] == word[-1]:
+	for i in words:
+		if len(i) > 1 and i[0] == i[-1]:
 			ctr += 1
-			lst.append(word)
+			lst.append(i)
 	
 	print("List of words with first and last character same\n", lst)
 	return ctr
 	
 result = match_words(['abc', 'cfc','xyz', 'aba', '1221','3883','7654'])
+
 print("Number of words having first and last character same:", result)
 
 
